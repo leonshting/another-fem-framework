0619b81d37bffc3aaa7aa35470e179c7c2e63c92
Leonid Shtanko <leonshting@gmail.com>
2017-11-08 13:06:16 +0300

    experiments continue

--- operator_assembler/matrix_assembler.py
+++ operator_assembler/matrix_assembler.py
@@ -23,45 +23,65 @@ class MatrixAssembler2D():
 
         self.dist_dict = {'lobatto': 'globatto', 'uniform': 'uniform'}
         self._matrices_h5_storages = {
-            # 'lobatto': '/home/lshtanko/Programming/another-fem-framework/datasources/globatto_matrices.h5',
-            'lobatto': '/Users/marusy/Programming/another-fem-framework/datasources/globatto_matrices.h5'
+            'lobatto': '/home/lshtanko/Programming/another-fem-framework/datasources/globatto_matrices.h5',
+            #'lobatto': '/Users/marusy/Programming/another-fem-framework/datasources/globatto_matrices.h5'
         }
 
         self._get_local_matrices_from_file('lobatto')
 
-        self.I_s2b = np.array([[0.94748435, 0.08218652, 0.00679549, -0.03646636],
-                               [0.31304669, 0.81263552, -0.09974331, -0.02593889],
-                               [-0.12478712, 1.00624854, 0.11348253, 0.00505606],
-                               [0.22008957, 0.05460024, 0.68518757, 0.04012262],
-                               [-0.01387373, 0.20320454, 0.7569224, 0.05374679],
-                               [-0.06389369, -0.06951784, 1.05398856, 0.07942297],
-                               [0.0598758, 0.04575923, -0.50042152, 1.39478649]])
+        #self.I_s2b = np.array([[0.94748435, 0.08218652, 0.00679549, -0.03646636],
+        #                       [0.31304669, 0.81263552, -0.09974331, -0.02593889],
+        #                       [-0.12478712, 1.00624854, 0.11348253, 0.00505606],
+        #                       [0.22008957, 0.05460024, 0.68518757, 0.04012262],
+        #                       [-0.01387373, 0.20320454, 0.7569224, 0.05374679],
+        #                       [-0.06389369, -0.06951784, 1.05398856, 0.07942297],
+        #                       [0.0598758, 0.04575923, -0.50042152, 1.39478649]])
+        #
+        #self.I_b2s = np.array([[4.73742176e-01, 7.82616722e-01, -3.11967812e-01,
+        #                        2.20089572e-01, -3.46843350e-02, -1.59734222e-01,
+        #                        2.99378982e-02],
+        #                       [8.21865191e-03, 4.06317760e-01, 5.03124268e-01,
+        #                        1.09200474e-02, 1.01602272e-01, -3.47589224e-02,
+        #                        4.57592318e-03],
+        #                       [6.79548951e-04, -4.98716573e-02, 5.67412656e-02,
+        #                        1.37037514e-01, 3.78461201e-01, 5.26994280e-01,
+        #                        -5.00421522e-02],
+        #                       [-1.82331802e-02, -6.48472355e-02, 1.26401418e-02,
+        #                        4.01226215e-02, 1.34366971e-01, 1.98557435e-01,
+        #                        6.97393247e-01]])
+        #self.I_s2b = np.array([[1.,0.],
+        #                       [0.5, 0.5],
+        #                       [0.,1.]])
+        #self.I_b2s = np.array([[0.5, 0.5, 0.],
+        #                       [0., 0.5, 0.5]])
+        self.I_b2s = np.array([[ 0.57288245,  0.59966694,  0.0802658 , -0.27140653,  0.01859134],
+                               [-0.02286845,  0.4179349 ,  0.2098671 ,  0.4179349 , -0.02286845],
+                               [ 0.01859134, -0.27140653,  0.0802658 ,  0.59966694,  0.57288245]])
 
-        self.I_b2s = np.array([[4.73742176e-01, 7.82616722e-01, -3.11967812e-01,
-                                2.20089572e-01, -3.46843350e-02, -1.59734222e-01,
-                                2.99378982e-02],
-                               [8.21865191e-03, 4.06317760e-01, 5.03124268e-01,
-                                1.09200474e-02, 1.01602272e-01, -3.47589224e-02,
-                                4.57592318e-03],
-                               [6.79548951e-04, -4.98716573e-02, 5.67412656e-02,
-                                1.37037514e-01, 3.78461201e-01, 5.26994280e-01,
-                                -5.00421522e-02],
-                               [-1.82331802e-02, -6.48472355e-02, 1.26401418e-02,
-                                4.01226215e-02, 1.34366971e-01, 1.98557435e-01,
-                                6.97393247e-01]])
+        self.I_s2b = np.array([[ 1.1457649 , -0.18294757,  0.03718267],
+                               [ 0.29983347,  0.83586979, -0.13570326],
+                               [ 0.0802658 ,  0.83946839,  0.0802658 ],
+                               [-0.13570326,  0.83586979,  0.29983347],
+                               [ 0.03718267, -0.18294757,  1.1457649 ]])
 
-    def _get_local_ff_matrix(self, distribution, order, cell):
+    def _get_local_ff_matrix(self, distribution, order, cell, diagonal=True):
         if self._ff_matrices.get((order, distribution, cell.size)) is None:
-            self._ff_matrices[(order, distribution, cell.size)] = local_funcfunc_matrix(dim=2,
-                                                                                        distribution=self.dist_dict[
-                                                                                            distribution], order=order)
-        return self._ff_matrices[(order, distribution, cell.size)]
+            self._ff_matrices[(order, distribution, cell.size)] = local_funcfunc_matrix(
+                dim=2,
+                distribution=self.dist_dict[distribution],
+                order=order,
+            )
+        if diagonal:
+            return np.diag(self._ff_matrices[(order, distribution, cell.size)].sum(axis=1))
+        else:
+            return self._ff_matrices[(order, distribution, (0,cell.size[0]))]
 
     def _get_local_gg_matrix(self, distribution, order):
+        #TODO: DANGER in zero_index
         if self._gg_matrices.get((order, distribution)) is None:
             self._gg_matrices[(order, distribution)] = local_gradgrad_matrix(dim=2,
                                                                              distribution=self.dist_dict[distribution],
-                                                                             order=order)
+                                                                             order=order)[0]
         return self._gg_matrices[(order, distribution)]
 
     def _get_local_matrices_from_file(self, distribution):
@@ -80,38 +100,40 @@ class MatrixAssembler2D():
 
         M_source.close()
 
-    def merge_two_cells(self, cell_1, cell_2):
+    def merge_two_cells(self, cell_1, cell_2, matrix='grad'):
         glob = csr_matrix((self.assembly_interface.get_ddof_count(), self.assembly_interface.get_ddof_count()))
         pairtuple_1 = self.assembly_interface.allocator.get_cell_list_of_ddofs(cell=cell_1)
         props_1 = self.assembly_interface.allocator.get_cell_props(cell_1)
         pairtuple_2 = self.assembly_interface.allocator.get_cell_list_of_ddofs(cell=cell_2)
         props_2 = self.assembly_interface.allocator.get_cell_props(cell_2)
-
+        print(pairtuple_1, pairtuple_2)
         dist_1 = distributed_eye(pairtuples=pairtuple_1,
                                  shape=(len(pairtuple_1), self.assembly_interface.get_ddof_count())).tocoo()
         dist_2 = distributed_eye(pairtuples=pairtuple_2,
                                  shape=(len(pairtuple_2), self.assembly_interface.get_ddof_count())).tocoo()
-
-        glob += dist_1.T * csr_matrix(self._get_local_gg_matrix(distribution=props_1[1], order=props_1[0])) * dist_1
-        glob += dist_2.T * csr_matrix(self._get_local_gg_matrix(distribution=props_2[1], order=props_2[0])) * dist_2
-        return glob
+        if matrix == 'grad':
+            glob += dist_1.T * csr_matrix(self._get_local_gg_matrix(distribution=props_1[1], order=props_1[0])) * dist_1
+            glob += dist_2.T * csr_matrix(self._get_local_gg_matrix(distribution=props_2[1], order=props_2[0])) * dist_2
+            return glob
+        elif matrix == 'mass':
+            glob += dist_1.T * csr_matrix(self._get_local_ff_matrix(distribution=props_1[1], order=props_1[0], cell=cell_1)) * dist_1
+            glob += dist_2.T * csr_matrix(self._get_local_ff_matrix(distribution=props_2[1], order=props_2[0], cell=cell_2)) * dist_2
+            return glob
 
     def _distribute_one_cell(self, cell: Cell2D):
-
         pairtuple = self.assembly_interface.allocator.get_cell_list_of_ddofs(cell=cell)
         props = self.assembly_interface.allocator.get_cell_props(cell)
         dist_1 = distributed_eye_easy(pairtuples=pairtuple, axis2shape=self.assembly_interface.get_ddof_count()).tocoo()
         glob = dist_1.T * csr_matrix(self._get_local_gg_matrix(distribution=props[1], order=props[0])) * dist_1
         return glob.tocsr()
 
-    # bad oine
     def _distribute_mass_one_cell(self, cell: Cell2D):
         pairtuple = self.assembly_interface.allocator.get_cell_list_of_ddofs(cell=cell)
         props = self.assembly_interface.allocator.get_cell_props(cell)
         dist_1 = distributed_eye_easy(pairtuples=pairtuple, axis2shape=self.assembly_interface.get_ddof_count()).tocoo()
         glob = dist_1.T * csr_matrix(self._get_local_ff_matrix(distribution=props[1],
                                                                order=props[0], cell=cell)) * dist_1
-        return glob.tocsr().sum(axis=1).toarray()
+        return glob.tocsr()
 
     # rewrite with consuming interpolants
     def _distribute_interpolant(self, left_dofs, right_dofs, interp):
@@ -142,14 +164,25 @@ class MatrixAssembler2D():
 
     def assemble(self, verbose=True):
         future_ignore = set()
-        glob_matrix = csr_matrix((self.assembly_interface.get_ddof_count(), self.assembly_interface.get_ddof_count()))
+        glob_matrix = csr_matrix((
+            self.assembly_interface.get_ddof_count(),
+            self.assembly_interface.get_ddof_count()))
+
+        glob_mass_matrix = csr_matrix((
+            self.assembly_interface.get_ddof_count(),
+            self.assembly_interface.get_ddof_count()))
+
         gather_evth = []
         for num, props in enumerate(self.assembly_interface.iterate_ddofs_and_wconn()):
             #bad way probably refactor
             if props['cell'].ll_vertex not in future_ignore:
                 peer_merged = csr_matrix(
                     (self.assembly_interface.get_ddof_count(), self.assembly_interface.get_ddof_count()))
+                peer_mass_merged = csr_matrix(
+                    (self.assembly_interface.get_ddof_count(), self.assembly_interface.get_ddof_count()))
+
                 host_local = self._distribute_one_cell(props['cell'])
+                mass_local = self._distribute_mass_one_cell(props['cell'])
 
                 # TODO: rewrite to multiple merge
                 host_dofs_to_merge = []
@@ -161,6 +194,9 @@ class MatrixAssembler2D():
 
                 for (k_edge, adj_cells), (w_edge, weak_conns) in zip(props['adj_cells'].items(), props['wconn'].items()):
                     peer_merged += self.merge_two_cells(cell_1=adj_cells[0][1], cell_2=adj_cells[1][1])
+                    peer_mass_merged += self.merge_two_cells(cell_1=adj_cells[0][1],
+                                                             cell_2=adj_cells[1][1],
+                                                             matrix='mass')
                     tmp_peer = self.stack_wcon_dofs(weak_conns)
                     tmp_host = self.assembly_interface.allocator.get_flat_list_of_ddofs_global(
                         edge=k_edge, cell=props['cell'])
@@ -206,10 +242,13 @@ class MatrixAssembler2D():
                                     peer_merged))
 
                 init_operator = peer_merged + host_local
+                init_mass = peer_mass_merged + mass_local
 
                 glob_matrix += whole_dist.T * init_operator * whole_dist
+                glob_mass_matrix += whole_dist.T * init_mass * whole_dist
 
             if verbose:
                 print('\r', num, end='')
         self.assembled = glob_matrix
+        self.assembled_mass = glob_mass_matrix
         return gather_evth
